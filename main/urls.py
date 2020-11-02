5fc04efc110573e2a1f45fe2dbb5c24835d46ff3
Ignacio Figueroa <nacho.al97@gmail.com>
2020-11-02 20:33:56 -0300

    login

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name='main'
 
 urlpatterns = [ 
-    path('', views.landingPage, name='landingPage' ),
-    path('index', views.index, name='index' ),
+    path('', views.index, name='index' ),
     path('cervezas.html/', views.cervezas, name='cervezas'),
     path('registro.html/', views.registro, name='registro')
 ]
--- main/views.py
+++ main/views.py
@@ -4,10 +4,6 @@ from django.template import loader
 # Create your views here.
 
 
-def landingPage(request):
-    return HttpResponse("Holi")
-
-
 def index(request):
     template = loader.get_template('main/index.html')
     context = {}
