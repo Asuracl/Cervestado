47b96fb76921e752840cf67d03a74de21c472a28
Javier Carrasco <zerodx231@gmail.com>
2020-11-08 18:56:56 -0300

    intento de registro numer 5000

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,8 @@ from .views import index, cervezas, registro, login
 urlpatterns = [ 
     path('', index, name='index' ),
     path('cervezas/', cervezas, name='cervezas'),
+    path('login/', login, name='login'),
     path('registro/', registro, name='registro'),
-    path('login/', login, name='login')
     
 ]
 
