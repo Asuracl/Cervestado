3753516b4a36daa56678fb2d917c76913b927c8a
Javier Carrasco <zerodx231@gmail.com>
2020-11-09 10:04:38 -0300

    ultimos cambios

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import index, cervezas, registro, login
 
 urlpatterns = [ 
+
     path('', index, name='index' ),
-    path('cervezas/', cervezas, name='cervezas'),
+    path('index/', index, name='index'),
     path('login/', login, name='login'),
+    path('cervezas/', cervezas, name='cervezas'),
     path('registro/', registro, name='registro'),
     
 ]
